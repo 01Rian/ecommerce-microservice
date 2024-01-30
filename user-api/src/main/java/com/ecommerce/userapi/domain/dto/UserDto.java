ecdb27c15bebad9a07caaefb5f532932caf23b70
Rian <riansantosjr11@gmail.com>
2024-01-30 17:03:25 -0300

    (user-api): add user domain

---
+++ user-api/src/main/java/com/ecommerce/userapi/domain/dto/UserDto.java
@@ -0,0 +1,14 @@
+package com.ecommerce.userapi.domain.dto;
+
+import java.time.LocalDateTime;
+
+public class UserDto {
+
+    private long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+}
---
+++ user-api/src/main/java/com/ecommerce/userapi/domain/entity/UserEntity.java
@@ -0,0 +1,22 @@
+package com.ecommerce.userapi.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private LocalDateTime dataCadastro;
+}
