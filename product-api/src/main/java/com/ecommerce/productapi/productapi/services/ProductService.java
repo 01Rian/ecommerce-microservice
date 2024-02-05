c6ebbc58b5dfd057e42a647264134dd4ada1933a
Rian <riansantosjr11@gmail.com>
2024-02-05 18:53:24 -0300

    (product-api): add product service

---
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/services/ProductService.java
@@ -0,0 +1,59 @@
+package com.ecommerce.productapi.productapi.services;
+
+import com.ecommerce.productapi.productapi.domain.dto.ProductDto;
+import com.ecommerce.productapi.productapi.domain.entities.ProductEntity;
+import com.ecommerce.productapi.productapi.mappers.impl.ProductMapper;
+import com.ecommerce.productapi.productapi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private ProductMapper mapper;
+
+    public List<ProductDto> getAll() {
+        List<ProductEntity> products = productRepository.findAll();
+        return products
+                .stream()
+                .map(mapper::mapTo)
+                .collect(Collectors.toList());
+    }
+
+    public List<ProductDto> getProductByCategoryId(Long categoryId) {
+        List<ProductEntity> products = productRepository.getProductByCategory(categoryId);
+        return products
+                .stream()
+                .map(mapper::mapTo)
+                .collect(Collectors.toList());
+    }
+
+    public ProductDto findByProductIdentifier(String identifier) {
+        ProductEntity product = productRepository.findByProductIdentifier(identifier);
+        if (product != null) {
+            return mapper.mapTo(product);
+        }
+        return null;
+    }
+
+    public ProductDto save(ProductDto productDto) {
+        productDto.setProductIdentifier(productDto.getProductIdentifier().toLowerCase());
+        productDto.setNome(productDto.getNome().toLowerCase());
+
+        ProductEntity product = productRepository.save(mapper.mapFrom(productDto));
+        return mapper.mapTo(product);
+    }
+
+    public void delete(long productId) {
+        Optional<ProductEntity> product = productRepository.findById(productId);
+        product.ifPresent(productEntity -> productRepository.delete(productEntity));
+    }
+}
