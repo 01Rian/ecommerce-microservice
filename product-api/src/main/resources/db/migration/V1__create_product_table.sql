c9a0dcee8c42f43de9d97dcbc4dea658abf68c00
Rian <riansantosjr11@gmail.com>
2024-02-06 19:22:46 -0300

    (product-api): rename db migration

--- product-api/src/main/resources/db/migration/V1__create_product_table.sql
+++ product-api/src/main/resources/db/migration/V1__create_product_table.sql
@@ -4,3 +4,16 @@ CREATE TABLE products.category (
     id bigserial PRIMARY KEY,
     nome VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE products.product (
+    id bigserial PRIMARY KEY,
+    product_identifier VARCHAR NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    descricao VARCHAR(100) NOT NULL,
+    preco FLOAT NOT NULL,
+    category_id bigint REFERENCES products.category(id)
+);
+
+INSERT INTO products.category(id, nome) VALUES(1, 'eletronico');
+INSERT INTO products.category(id, nome) VALUES(2, 'moveis');
+INSERT INTO products.category(id, nome) VALUES(3, 'brinquedos');
