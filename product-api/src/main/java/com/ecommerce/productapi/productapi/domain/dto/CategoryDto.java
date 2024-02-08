1012d99b75ce922c6009f6884c477264b62bc9b2
Rian <riansantosjr11@gmail.com>
2024-02-08 17:06:09 -0300

    (product-api): refactor CategoryDto to add name validation

--- product-api/src/main/java/com/ecommerce/productapi/productapi/domain/dto/CategoryDto.java
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/domain/dto/CategoryDto.java
@@ -1,5 +1,6 @@
 package com.ecommerce.productapi.productapi.domain.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,5 +15,6 @@ public class CategoryDto {
 
     @NotNull
     private long id;
+    @NotBlank
     private String nome;
 }
