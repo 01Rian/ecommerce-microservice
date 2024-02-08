f429ad682173c1f2cf1c52036a70660907544b02
Rian <riansantosjr11@gmail.com>
2024-02-08 16:36:08 -0300

    (product-api): rename resource mapping

--- product-api/src/main/java/com/ecommerce/productapi/productapi/controllers/ProductController.java
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/controllers/ProductController.java
@@ -15,28 +15,28 @@ public class ProductController {
     @Autowired
     private ProductService productService;
 
-    @GetMapping("/product")
+    @GetMapping("/products")
     public List<ProductDto> getProducts() {
         return productService.getAll();
     }
 
-    @GetMapping("/product/category/{id}")
+    @GetMapping("/products/category/{id}")
     public List<ProductDto> getProductByCategory(@PathVariable("id") long categoryId) {
         return productService.getProductByCategoryId(categoryId);
     }
 
-    @GetMapping("/product/{identifier}")
+    @GetMapping("/products/{identifier}")
     public ProductDto findById(@PathVariable("identifier") String productIdentifier) {
         return productService.findByProductIdentifier(productIdentifier);
     }
 
-    @PostMapping("/product")
+    @PostMapping("/products")
     @ResponseStatus(HttpStatus.CREATED)
     public ProductDto newProduct(@Valid @RequestBody ProductDto productDto) {
         return productService.save(productDto);
     }
 
-    @DeleteMapping("/product/{id}")
+    @DeleteMapping("/products/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable("id") long id) {
         productService.delete(id);
