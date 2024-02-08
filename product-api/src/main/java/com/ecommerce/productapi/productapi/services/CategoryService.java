f8af58fd09f94660d59c3a6a16bf1145b68362e3
Rian <riansantosjr11@gmail.com>
2024-02-08 17:06:55 -0300

    (product-api): add CategoryService implementation

---
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/services/CategoryService.java
@@ -0,0 +1,36 @@
+package com.ecommerce.productapi.productapi.services;
+
+import com.ecommerce.productapi.productapi.domain.dto.CategoryDto;
+import com.ecommerce.productapi.productapi.domain.entities.CategoryEntity;
+import com.ecommerce.productapi.productapi.mappers.impl.CategoryMapper;
+import com.ecommerce.productapi.productapi.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CategoryMapper mapper;
+
+    public List<CategoryDto> getAll() {
+        List<CategoryEntity> categories = categoryRepository.findAll();
+        return categories
+                .stream()
+                .map(mapper::mapTo)
+                .collect(Collectors.toList());
+    }
+
+    public CategoryDto save(CategoryDto categoryDto) {
+        categoryDto.setNome(categoryDto.getNome().toLowerCase());
+
+        CategoryEntity category = categoryRepository.save(mapper.mapFrom(categoryDto));
+        return  mapper.mapTo(category);
+    }
+}
