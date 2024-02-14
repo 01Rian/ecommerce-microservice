00929a95115dd6fd813dc33f50239fadac8bfffb
Rian <riansantosjr11@gmail.com>
2024-02-14 16:58:18 -0300

    (shopping-api): add getShopsByFilter and getReportByDate controller implementation

--- shopping-api/src/main/java/com/ecommerce/shoppingapi/controllers/ShopController.java
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/controllers/ShopController.java
@@ -1,12 +1,15 @@
 package com.ecommerce.shoppingapi.controllers;
 
 import com.ecommerce.shoppingapi.domain.dto.ShopDto;
+import com.ecommerce.shoppingapi.domain.dto.ShopReportDto;
 import com.ecommerce.shoppingapi.services.ShopService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @RestController
@@ -33,6 +36,29 @@ public class ShopController {
     @GetMapping("/shoppings/{id}")
     public ShopDto findById(@PathVariable("id") Long id) {
         return shopService.findById(id);
+    }
+
+    @GetMapping("/shoppings/search")
+    public List<ShopDto> getShopsByFilter
+            (
+            @RequestParam(name = "startDate", required = true)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate startDate,
+            @RequestParam(name = "endDate", required = false)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate endDate,
+            @RequestParam(name = "maxValue", required = false) Float maxValue
+            ) {
+        return shopService.getShopsByFilter(startDate, endDate, maxValue);
+    }
+
+    @GetMapping("/shoppings/report")
+    public ShopReportDto getReportByDate
+            (
+            @RequestParam(name = "startDate", required = true)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate startDate,
+            @RequestParam(name = "endDate", required = true)
+            @DateTimeFormat(pattern = "dd/MM/yyyy") LocalDate endDate
+            ) {
+        return shopService.getReportByDate(startDate, endDate);
     }
 
     @PostMapping("/shoppings")
