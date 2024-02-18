217dc26a617a281188ceafecb78b4c6667ad947f
Rian <riansantosjr11@gmail.com>
2024-02-18 15:29:55 -0300

    (user-api): refactor UserService to implement exceptions

--- user-api/src/main/java/com/ecommerce/userapi/service/UserService.java
+++ user-api/src/main/java/com/ecommerce/userapi/service/UserService.java
@@ -2,25 +2,31 @@ package com.ecommerce.userapi.service;
 
 import com.ecommerce.userapi.domain.dto.UserDto;
 import com.ecommerce.userapi.domain.entity.UserEntity;
-import com.ecommerce.userapi.mapper.MapperImpl;
+import com.ecommerce.userapi.exception.UserBadRequestException;
+import com.ecommerce.userapi.exception.UserNotFoundException;
+import com.ecommerce.userapi.mapper.impl.MapperImpl;
 import com.ecommerce.userapi.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final MapperImpl mapper;
 
     @Autowired
-    private MapperImpl mapper;
+    public UserService(UserRepository userRepository, MapperImpl mapper) {
+        this.userRepository = userRepository;
+        this.mapper = mapper;
+    }
 
+    @Transactional(readOnly = true)
     public List<UserDto> getAll() {
         List<UserEntity> users = userRepository.findAll();
         return users.stream()
@@ -28,32 +34,43 @@ public class UserService {
                 .collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public UserDto findById(Long userId) {
-        Optional<UserEntity> users = userRepository.findById(userId);
-        return users.map(userEntity -> mapper.mapTo(userEntity)).orElse(null);
-    }
-
-    public UserDto save(UserDto userDto) {
-        userDto.setNome(userDto.getNome().toLowerCase());
-        userDto.setDataCadastro(LocalDateTime.now());
-
-        UserEntity user = userRepository.save(mapper.mapFrom(userDto));
+        UserEntity user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
         return mapper.mapTo(user);
     }
 
-    public void delete(long userId) {
-        Optional<UserEntity> user = userRepository.findById(userId);
-        user.ifPresent(userEntity -> userRepository.delete(userEntity));
+    @Transactional
+    public UserDto save(UserDto userDto) {
+        UserEntity ifExist = userRepository.findByCpf(userDto.getCpf());
+
+        if (ifExist != null) {
+            throw new UserBadRequestException();
+        }
+
+        userDto.setNome(userDto.getNome().toLowerCase());
+        userDto.setDataCadastro(LocalDateTime.now());
+        UserEntity user = userRepository.save(mapper.mapFrom(userDto));
+
+        return mapper.mapTo(user);
     }
 
+    @Transactional
+    public void delete(Long userId) throws UserNotFoundException {
+        UserEntity user = userRepository.findById(userId).orElseThrow(UserNotFoundException::new);
+        userRepository.delete(user);
+    }
+
+    @Transactional(readOnly = true)
     public UserDto findByCpf(String cpf) {
         UserEntity user = userRepository.findByCpf(cpf);
         if (user != null) {
             return mapper.mapTo(user);
         }
-        return null;
+         throw new UserNotFoundException();
     }
 
+    @Transactional(readOnly = true)
     public List<UserDto> queryByName(String name) {
         List<UserEntity> users = userRepository.queryByNomeLike(name);
         return users.stream()
