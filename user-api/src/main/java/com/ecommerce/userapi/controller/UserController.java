3908110cb5cc01c6054f84e557b59f725e78073f
Rian <riansantosjr11@gmail.com>
2024-02-18 15:36:34 -0300

    (user-api): add getUsersPage controller to implement pagination request

--- user-api/src/main/java/com/ecommerce/userapi/controller/UserController.java
+++ user-api/src/main/java/com/ecommerce/userapi/controller/UserController.java
@@ -4,6 +4,9 @@ import com.ecommerce.userapi.domain.dto.UserDto;
 import com.ecommerce.userapi.service.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +21,18 @@ public class UserController {
     @GetMapping("/users")
     public List<UserDto> getUsers() {
         return userService.getAll();
+    }
+
+    @GetMapping("/users/pageable")
+    public Page<UserDto> getUsersPage(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return userService.getAllPage(pageRequest);
     }
 
     @GetMapping("/users/{id}")
