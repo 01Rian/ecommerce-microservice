a5436b37a33e0560d48164a1ebca7b458d92bf61
Rian <riansantosjr11@gmail.com>
2024-02-19 18:02:25 -0300

    (product-api): add ErrorDto

---
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/domain/dto/ErrorDto.java
@@ -0,0 +1,19 @@
+package com.ecommerce.productapi.productapi.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorDto {
+
+    private Integer status;
+    private String message;
+    private LocalDateTime timestamp;
+}
