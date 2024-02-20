ed938bee58f7ed65ca2f3a9d719c2300cee9f6ed
Rian <riansantosjr11@gmail.com>
2024-02-20 17:05:46 -0300

    (product-api): add getAllPageProducts controller to implement pagination request

--- product-api/src/main/java/com/ecommerce/productapi/productapi/controllers/ProductController.java
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/controllers/ProductController.java
@@ -4,6 +4,9 @@ import com.ecommerce.productapi.productapi.domain.dto.ProductDto;
 import com.ecommerce.productapi.productapi.services.ProductService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +21,18 @@ public class ProductController {
     @GetMapping("/products")
     public List<ProductDto> getAllProducts() {
         return productService.getAllProducts();
+    }
+
+    @GetMapping("/products/pageable")
+    public Page<ProductDto> getAllPageProducts(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "nome") String orderBy
+    ) {
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+
+        return productService.getAllPageProducts(pageRequest);
     }
 
     @GetMapping("/products/category/{id}")
