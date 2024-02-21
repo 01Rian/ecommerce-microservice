378e42c00453205b829f05b2fc5f2f7465ecb776
Rian <riansantosjr11@gmail.com>
2024-02-21 15:25:26 -0300

    (product-api): refactor ProductControllerAdvice to add ArgumentNotValidException

--- product-api/src/main/java/com/ecommerce/productapi/productapi/exception/advice/ProductControllerAdvice.java
+++ product-api/src/main/java/com/ecommerce/productapi/productapi/exception/advice/ProductControllerAdvice.java
@@ -5,12 +5,16 @@ import com.ecommerce.productapi.productapi.exception.CategoryNotFoundException;
 import com.ecommerce.productapi.productapi.exception.ProductAlreadyExistsException;
 import com.ecommerce.productapi.productapi.exception.ProductNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @ControllerAdvice(basePackages = "com.ecommerce.productapi.productapi.controllers")
 public class ProductControllerAdvice {
@@ -43,7 +47,28 @@ public class ProductControllerAdvice {
     public ErrorDto handleProductAlreadyExistsException(ProductAlreadyExistsException exception) {
         ErrorDto errorDto = new ErrorDto();
         errorDto.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorDto.setMessage("Product Already Exists");
+        errorDto.setMessage("Product Identifier Already Exists");
+        errorDto.setTimestamp(LocalDateTime.now());
+        return errorDto;
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ErrorDto handleArgumentNotValidException(MethodArgumentNotValidException exception) {
+        ErrorDto errorDto = new ErrorDto();
+        errorDto.setStatus(HttpStatus.BAD_REQUEST.value());
+
+        BindingResult result = exception.getBindingResult();
+        List<FieldError> fieldErrors = result.getFieldErrors();
+
+        StringBuilder sb = new StringBuilder("Invalid Value For Field(s):");
+        for (FieldError fieldError : fieldErrors) {
+            sb.append(" ");
+            sb.append("[").append(fieldError.getField()).append("]");
+        }
+
+        errorDto.setMessage(sb.toString());
         errorDto.setTimestamp(LocalDateTime.now());
         return errorDto;
     }
