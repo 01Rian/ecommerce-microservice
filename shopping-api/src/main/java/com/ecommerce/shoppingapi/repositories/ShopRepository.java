69289b5290fab257700e21f18275ca691d56fa06
Rian <riansantosjr11@gmail.com>
2024-02-23 16:40:37 -0300

    (shopping-api): refactor ShopRepository to remove findAllByDateGreaterThan

--- shopping-api/src/main/java/com/ecommerce/shoppingapi/repositories/ShopRepository.java
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/repositories/ShopRepository.java
@@ -4,13 +4,9 @@ import com.ecommerce.shoppingapi.domain.entities.ShopEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface ShopRepository extends JpaRepository<ShopEntity, Long>, ReportRepository {
-
      List<ShopEntity> findAllByUserIdentifier(String userIdentifier);
-
-     List<ShopEntity> findAllByDateGreaterThan(LocalDateTime date);
 }
