56e456de01786f83997f5f30f179c259d7a538c1
Rian <riansantosjr11@gmail.com>
2024-02-29 17:30:55 -0300

    (shopping-api): refactor mapper to rename entity

--- shopping-api/src/main/java/com/ecommerce/shoppingapi/mappers/impl/ShopMapper.java
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/mappers/impl/ShopMapper.java
@@ -1,7 +1,7 @@
 package com.ecommerce.shoppingapi.mappers.impl;
 
 import com.ecommerce.shoppingapi.domain.dto.ShopDto;
-import com.ecommerce.shoppingapi.domain.entities.ShopEntity;
+import com.ecommerce.shoppingapi.domain.entities.Shop;
 import com.ecommerce.shoppingapi.mappers.Mapper;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
@@ -9,17 +9,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class ShopMapper implements Mapper<ShopEntity, ShopDto> {
+public class ShopMapper implements Mapper<Shop, ShopDto> {
 
     private final ModelMapper modelMapper;
 
     @Override
-    public ShopDto mapTo(ShopEntity shopEntity) {
-        return modelMapper.map(shopEntity, ShopDto.class);
+    public ShopDto mapTo(Shop shop) {
+        return modelMapper.map(shop, ShopDto.class);
     }
 
     @Override
-    public ShopEntity mapFrom(ShopDto shopDto) {
-        return modelMapper.map(shopDto, ShopEntity.class);
+    public Shop mapFrom(ShopDto shopDto) {
+        return modelMapper.map(shopDto, Shop.class);
     }
 }
