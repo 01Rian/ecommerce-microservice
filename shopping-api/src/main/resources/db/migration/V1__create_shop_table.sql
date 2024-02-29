101b427630e64cabd0be1494e4761b49ddc224b2
Rian <riansantosjr11@gmail.com>
2024-02-29 17:31:42 -0300

    (shopping-api): refactor shop table schema

--- shopping-api/src/main/resources/db/migration/V1__create_shop_table.sql
+++ shopping-api/src/main/resources/db/migration/V1__create_shop_table.sql
@@ -1,14 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS shopping;
 
 CREATE TABLE shopping.shop (
-    id bigserial PRIMARY KEY,
-    user_identifier VARCHAR(100) NOT NULL,
+    id BIGSERIAL PRIMARY KEY,
+    user_identifier VARCHAR(50) NOT NULL,
     date TIMESTAMP NOT NULL,
     total FLOAT NOT NULL
 );
 
 CREATE TABLE shopping.item (
-    shop_id bigserial REFERENCES shopping.shop(id),
-    product_identifier VARCHAR(100) NOT NULL,
+    shop_id BIGSERIAL REFERENCES shopping.shop(id),
+    product_identifier VARCHAR NOT NULL,
     price FLOAT NOT NULL
 );
