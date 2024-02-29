c0f44c50ad330a23df5c510124a984efa6bcd3ba
Rian <riansantosjr11@gmail.com>
2024-02-29 17:28:51 -0300

    (shopping-api): refactor domain to rename entities

---
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/domain/entities/Shop.java
@@ -0,0 +1,31 @@
+package com.ecommerce.shoppingapi.domain.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "shop")
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userIdentifier;
+    private Float total;
+    private LocalDateTime date;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "item", joinColumns = @JoinColumn(name = "shop_id"))
+    private List<Item> items;
+}
