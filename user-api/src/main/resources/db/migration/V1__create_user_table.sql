a2ed2355e031d9b682ded105982651d2f7b93758
Rian <riansantosjr11@gmail.com>
2024-03-02 16:13:52 -0300

    (user-api): refactor users table schema

--- user-api/src/main/resources/db/migration/V1__create_user_table.sql
+++ user-api/src/main/resources/db/migration/V1__create_user_table.sql
@@ -1,11 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS users;
 
 CREATE TABLE users.user (
-    id bigserial PRIMARY KEY,
-    nome VARCHAR(100) NOT NULL,
-    cpf VARCHAR(100) NOT NULL,
-    endereco VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    telefone VARCHAR(100) NOT NULL,
-    data_cadastro TIMESTAMP NOT NULL
+    id BIGSERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    cpf VARCHAR(11) NOT NULL,
+    address VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL,
+    phone VARCHAR(50) NOT NULL,
+    data_register TIMESTAMP NOT NULL
 );
