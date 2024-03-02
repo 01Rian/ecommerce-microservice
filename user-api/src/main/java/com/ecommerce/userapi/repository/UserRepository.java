b500d0e88c9a33781e17adb6fbf239e181809b9c
Rian <riansantosjr11@gmail.com>
2024-03-02 16:13:06 -0300

    (user-api): refactor repository to rename entity

--- user-api/src/main/java/com/ecommerce/userapi/repository/UserRepository.java
+++ user-api/src/main/java/com/ecommerce/userapi/repository/UserRepository.java
@@ -1,15 +1,15 @@
 package com.ecommerce.userapi.repository;
 
-import com.ecommerce.userapi.domain.entity.UserEntity;
+import com.ecommerce.userapi.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    UserEntity findByCpf(String cpf);
+    User findByCpf(String cpf);
 
-    List<UserEntity> queryByNomeLike(String name);
+    List<User> queryByNameLike(String name);
 }
