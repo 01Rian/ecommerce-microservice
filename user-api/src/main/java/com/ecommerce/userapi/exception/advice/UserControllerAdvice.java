bc374ec5166524c967b31ad2421d942af3888e82
Rian <riansantosjr11@gmail.com>
2024-03-03 16:30:51 -0300

    (user-api): refactor exception to replace ControllerAdvice to RestControllerAdvice and add ErrorDto builder

--- user-api/src/main/java/com/ecommerce/userapi/exception/advice/UserControllerAdvice.java
+++ user-api/src/main/java/com/ecommerce/userapi/exception/advice/UserControllerAdvice.java
@@ -4,35 +4,30 @@ import com.ecommerce.userapi.domain.dto.ErrorDto;
 import com.ecommerce.userapi.exception.UserAlreadyExistsException;
 import com.ecommerce.userapi.exception.UserNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice(basePackages = "com.ecommerce.userapi.controller")
+@RestControllerAdvice(basePackages = "com.ecommerce.userapi.controller")
 public class UserControllerAdvice {
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(UserNotFoundException.class)
-    public ErrorDto handleUserNotFound(UserNotFoundException exception) {
-        ErrorDto errorDto = new ErrorDto();
-        errorDto.setStatus(HttpStatus.NOT_FOUND.value());
-        errorDto.setMessage("User Not Found");
-        errorDto.setTimestamp(LocalDateTime.now());
-        return errorDto;
+    public ErrorDto handleUserNotFoundException(UserNotFoundException exception) {
+        return ErrorDto.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message("User Not Found")
+                .timestamp(LocalDateTime.now())
+                .build();
     }
 
-    @ResponseBody
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ErrorDto handleUserAlreadyExistsException(UserAlreadyExistsException exception) {
-        ErrorDto errorDto = new ErrorDto();
-        errorDto.setStatus(HttpStatus.BAD_REQUEST.value());
-        errorDto.setMessage("User Already Exists");
-        errorDto.setTimestamp(LocalDateTime.now());
-        return errorDto;
+        return ErrorDto.builder()
+                .status(HttpStatus.NOT_FOUND.value())
+                .message("User Already Exists")
+                .timestamp(LocalDateTime.now())
+                .build();
     }
 }
