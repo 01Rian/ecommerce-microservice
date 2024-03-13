463943e68d5f6ae8bc34368e15c0fa765ba4a95c
Rian <riansantosjr11@gmail.com>
2024-03-12 23:08:51 -0300

    add script deploy and down to manage docker build

---
+++ down.py
@@ -0,0 +1,16 @@
+import os
+
+script_dir = os.path.dirname(__file__)
+
+base_dirs = [os.path.join(script_dir, "product-api/"),
+             os.path.join(script_dir, "shopping-api/"),
+             os.path.join(script_dir, "user-api/")]
+
+for base_dir in base_dirs:
+    compose_file = os.path.join(base_dir, "docker-compose.yml")
+
+    if os.path.exists(compose_file):
+        print(f"Down containers for {compose_file}")
+        os.system(f"docker compose -f {compose_file} down")
+    else:
+        print(f"Docker-compose.yml File Not Found In {base_dir}")
