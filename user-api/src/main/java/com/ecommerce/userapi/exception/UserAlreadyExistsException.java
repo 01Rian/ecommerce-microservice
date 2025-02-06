d1b171e6e593b80f3b952d7802b097e6591e26a9
Rian <riansantosjr11@gmail.com>
2025-02-05 23:45:46 -0300

    (user-api): Extend user exceptions to use base exception classes

--- user-api/src/main/java/com/ecommerce/userapi/exception/UserAlreadyExistsException.java
+++ user-api/src/main/java/com/ecommerce/userapi/exception/UserAlreadyExistsException.java
@@ -1,4 +1,9 @@
 package com.ecommerce.userapi.exception;
 
-public class UserAlreadyExistsException extends RuntimeException{
+public class UserAlreadyExistsException extends ResourceConflictException {
+    private static final String RESOURCE_NAME = "Usuário";
+
+    public UserAlreadyExistsException(String fieldName, Object fieldValue) {
+        super(RESOURCE_NAME, fieldName, fieldValue);
+    }
 }
--- user-api/src/main/java/com/ecommerce/userapi/exception/UserNotFoundException.java
+++ user-api/src/main/java/com/ecommerce/userapi/exception/UserNotFoundException.java
@@ -1,4 +1,9 @@
 package com.ecommerce.userapi.exception;
 
-public class UserNotFoundException extends RuntimeException{
+public class UserNotFoundException extends ResourceNotFoundException {
+    private static final String RESOURCE_NAME = "Usuário";
+
+    public UserNotFoundException(String fieldName, Object fieldValue) {
+        super(RESOURCE_NAME, fieldName, fieldValue);
+    }
 }
