50599b6196f46a70374f3d0a0942ac6c73e8930d
Rian <riansantosjr11@gmail.com>
2025-02-06 14:19:29 -0300

    (user-api): update MapperImpl to use UserRequestDto and UserResponseDto

--- user-api/src/main/java/com/ecommerce/userapi/mapper/impl/MapperImpl.java
+++ user-api/src/main/java/com/ecommerce/userapi/mapper/impl/MapperImpl.java
@@ -1,25 +1,25 @@
 package com.ecommerce.userapi.mapper.impl;
 
-import com.ecommerce.userapi.domain.dto.UserDto;
+import com.ecommerce.userapi.domain.dto.UserRequestDto;
+import com.ecommerce.userapi.domain.dto.UserResponseDto;
 import com.ecommerce.userapi.domain.entity.User;
-import com.ecommerce.userapi.mapper.Mapper;
+
 import lombok.RequiredArgsConstructor;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Component
-public class MapperImpl implements Mapper<User, UserDto> {
+public class MapperImpl {
 
     private final ModelMapper modelMapper;
 
-    @Override
-    public UserDto mapTo(User user) {
-        return modelMapper.map(user, UserDto.class);
+    public UserResponseDto mapTo(User user) {
+        return modelMapper.map(user, UserResponseDto.class);
     }
 
-    @Override
-    public User mapFrom(UserDto userDto) {
-        return modelMapper.map(userDto, User.class);
+    public User mapFrom(UserRequestDto userRequestDto) {
+        return modelMapper.map(userRequestDto, User.class);
     }
 }
