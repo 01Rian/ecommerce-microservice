c8decd01eb597286bb393e25c2bb0e3ce456ecb2
Rian <riansantosjr11@gmail.com>
2025-02-13 12:02:10 -0300

    (product-api): enhance Product entity with audit fields and quantity

--- product-api/src/main/java/com/ecommerce/productapi/domain/entities/Product.java
+++ product-api/src/main/java/com/ecommerce/productapi/domain/entities/Product.java
@@ -6,23 +6,44 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 @Entity
-@Table(name = "product")
+@Table(name = "product", schema = "products")
 public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long    id;
     private String  name;
-    private Float   price;
+    private BigDecimal price;
     private String  description;
+    private Integer quantity;
     private String  productIdentifier;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
     private Category category;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
 }
