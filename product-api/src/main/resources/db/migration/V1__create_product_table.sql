f705494a17db0f456f0cb0f44ac27dc449458eb4
Rian <riansantosjr11@gmail.com>
2025-02-14 12:18:20 -0300

    (product-api): change flyway migration files
    change schema migration files with "" to avoid errors in flyway migration in the test environment on the h2 database

--- product-api/src/main/resources/db/migration/V1__create_product_table.sql
+++ product-api/src/main/resources/db/migration/V1__create_product_table.sql
@@ -1,11 +1,11 @@
 CREATE SCHEMA IF NOT EXISTS products;
 
-CREATE TABLE products."category" (
+CREATE TABLE products.category (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
 
-CREATE TABLE products."product" (
+CREATE TABLE products.product (
     id BIGSERIAL PRIMARY KEY,
     product_identifier VARCHAR NOT NULL,
     name VARCHAR(50) NOT NULL,
--- product-api/src/main/resources/db/migration/V2__add_columns_to_product_and_category_tables.sql
+++ product-api/src/main/resources/db/migration/V2__add_columns_to_product_and_category_tables.sql
@@ -1,11 +1,11 @@
-ALTER TABLE products."category" ADD description VARCHAR(255);
-ALTER TABLE products."category" ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE products."category" ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE products.category ADD description VARCHAR(255);
+ALTER TABLE products.category ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE products.category ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
 
-ALTER TABLE products."product" ADD quantity INTEGER;
-ALTER TABLE products."product" ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE products."product" ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-ALTER TABLE products."product" ADD CONSTRAINT uk_product_identifier UNIQUE (product_identifier);
+ALTER TABLE products.product ADD quantity INTEGER;
+ALTER TABLE products.product ADD created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE products.product ADD updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE products.product ADD CONSTRAINT uk_product_identifier UNIQUE (product_identifier);
 
 -- Atualiza as colunas created_at e updated_at para os registros existentes
-UPDATE products."product" SET created_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP;
+UPDATE products.product SET created_at = CURRENT_TIMESTAMP, updated_at = CURRENT_TIMESTAMP;
