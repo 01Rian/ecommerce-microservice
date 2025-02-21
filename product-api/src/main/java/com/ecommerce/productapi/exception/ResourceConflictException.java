98f36a4149b1a5a2a1c44c17cde005f4a956ad24
Rian <riansantosjr11@gmail.com>
2025-02-21 15:48:24 -0300

    (product-api): add ResourceConflictException and ResourceNotFoundException for improved error handling

---
+++ product-api/src/main/java/com/ecommerce/productapi/exception/ResourceConflictException.java
@@ -0,0 +1,22 @@
+package com.ecommerce.productapi.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ResourceConflictException extends BaseException {
+  private static final String DEFAULT_MESSAGE = "Conflito de recurso";
+  private static final HttpStatus DEFAULT_STATUS = HttpStatus.CONFLICT;
+  private static final String ERROR_CODE = "RESOURCE_CONFLICT";
+
+  public ResourceConflictException() {
+    super(DEFAULT_MESSAGE, DEFAULT_STATUS, ERROR_CODE);
+  }
+
+  public ResourceConflictException(String message) {
+    super(message, DEFAULT_STATUS, ERROR_CODE);
+  }
+
+  public ResourceConflictException(String resourceName, String fieldName, Object fieldValue) {
+    super(String.format("%s já existe com %s: '%s'", resourceName, fieldName, fieldValue),
+        DEFAULT_STATUS, ERROR_CODE);
+  }
+}
