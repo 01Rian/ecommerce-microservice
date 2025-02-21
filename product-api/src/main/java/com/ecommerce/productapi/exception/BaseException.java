e1ede91db6a988ab8dcf912c92d859b2f7d418a3
Rian <riansantosjr11@gmail.com>
2025-02-21 15:47:47 -0300

    (product-api): enhance BaseException to include status and errorCode for improved error handling

--- product-api/src/main/java/com/ecommerce/productapi/exception/BaseException.java
+++ product-api/src/main/java/com/ecommerce/productapi/exception/BaseException.java
@@ -1,11 +1,29 @@
 package com.ecommerce.productapi.exception;
 
+import org.springframework.http.HttpStatus;
+
 public abstract class BaseException extends RuntimeException {
-    public BaseException(String message) {
+    private final String message;
+    private final HttpStatus status;
+    private final String errorCode;
+
+    protected BaseException(String message, HttpStatus status, String errorCode) {
         super(message);
+        this.message = message;
+        this.status = status;
+        this.errorCode = errorCode;
     }
 
-    public BaseException(String message, Throwable cause) {
-        super(message, cause);
+    @Override
+    public String getMessage() {
+        return message;
     }
-} 
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+}
