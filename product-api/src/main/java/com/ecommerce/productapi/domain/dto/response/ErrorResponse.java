c906e0793452d71df3cfde13eeb6a4ae07c6a33d
Rian <riansantosjr11@gmail.com>
2025-02-21 15:46:35 -0300

    (product-api): add errorCode field to ErrorResponse for enhanced error handling

--- product-api/src/main/java/com/ecommerce/productapi/domain/dto/response/ErrorResponse.java
+++ product-api/src/main/java/com/ecommerce/productapi/domain/dto/response/ErrorResponse.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 public class ErrorResponse {
     private int status;
     private String message;
+    private String errorCode;
     
     @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime timestamp;
