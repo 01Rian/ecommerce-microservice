78188dab0cd66c5d3e25c4d6fdcd0da22b3eb6ad
Rian <riansantosjr11@gmail.com>
2025-02-21 15:50:23 -0300

    (product-api): refactor exceptions to extend ResourceNotFoundException and remove unused ProductAlreadyExistsException

--- product-api/src/main/java/com/ecommerce/productapi/exception/CategoryNotFoundException.java
+++ product-api/src/main/java/com/ecommerce/productapi/exception/CategoryNotFoundException.java
@@ -1,26 +1,9 @@
 package com.ecommerce.productapi.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+public class CategoryNotFoundException extends ResourceNotFoundException {
+    private static final String RESOURCE_NAME = "Categoria";
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class CategoryNotFoundException extends BaseException {
-    
-    private static final String DEFAULT_MESSAGE = "Categoria não encontrada";
-    
-    public CategoryNotFoundException() {
-        super(DEFAULT_MESSAGE);
-    }
-    
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-    
-    public CategoryNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-    
-    public CategoryNotFoundException(Long id) {
-        super(String.format("Categoria não encontrada com ID: %d", id));
+    public CategoryNotFoundException(String fieldName, Object fieldValue) {
+        super(RESOURCE_NAME, fieldName, fieldValue);
     }
 }
--- product-api/src/main/java/com/ecommerce/productapi/exception/ProductNotFoundException.java
+++ product-api/src/main/java/com/ecommerce/productapi/exception/ProductNotFoundException.java
@@ -1,26 +1,9 @@
 package com.ecommerce.productapi.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+public class ProductNotFoundException extends ResourceNotFoundException {
+    private static final String RESOURCE_NAME = "Produto";
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ProductNotFoundException extends BaseException {
-    
-    private static final String DEFAULT_MESSAGE = "Produto não encontrado";
-    
-    public ProductNotFoundException() {
-        super(DEFAULT_MESSAGE);
-    }
-    
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-    
-    public ProductNotFoundException(String message, Throwable cause) {
-        super(message, cause);
-    }
-    
-    public ProductNotFoundException(Long id) {
-        super(String.format("Produto não encontrado com ID: %d", id));
+    public ProductNotFoundException(String fieldName, Object fieldValue) {
+        super(RESOURCE_NAME, fieldName, fieldValue);
     }
 }
