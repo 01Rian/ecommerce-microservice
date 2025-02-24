a4bd5fe325fc8e6fac9d2f9ceeeae0ce545e293a
Rian <riansantosjr11@gmail.com>
2025-02-24 15:30:33 -0300

    (shopping-api): refactor ShoppingNotFoundException to extend ResourceNotFoundException

--- shopping-api/src/main/java/com/ecommerce/shoppingapi/exception/ShoppingNotFoundException.java
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/exception/ShoppingNotFoundException.java
@@ -1,4 +1,9 @@
 package com.ecommerce.shoppingapi.exception;
 
-public class ShoppingNotFoundException extends RuntimeException{
+public class ShoppingNotFoundException extends ResourceNotFoundException {
+  private static final String RESOURCE_NAME = "Shopping";
+
+  public ShoppingNotFoundException(String fieldName, Object fieldValue) {
+      super(RESOURCE_NAME, fieldName, fieldValue);
+  }
 }
