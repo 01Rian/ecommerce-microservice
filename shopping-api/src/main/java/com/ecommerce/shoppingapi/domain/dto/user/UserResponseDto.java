50ccb6ae3bcc6a2cde509d84ae82701513f444a1
Rian <riansantosjr11@gmail.com>
2025-02-24 14:21:55 -0300

    (shopping-api): refactor UserDto
    create UserResponseDto for user data representation in shopping API

--- shopping-api/src/main/java/com/ecommerce/shoppingapi/domain/dto/user/UserResponseDto.java
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/domain/dto/user/UserResponseDto.java
@@ -1,7 +1,5 @@
 package com.ecommerce.shoppingapi.domain.dto.user;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,22 +7,20 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
-public class UserDto {
+import com.fasterxml.jackson.annotation.JsonFormat;
 
-    @NotBlank
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDto {
+    private Long id;
     private String name;
-    @NotBlank
     private String cpf;
-    @NotBlank
-    private String address;
-    @NotBlank
     private String email;
-    @NotBlank
     private String phone;
-    @NotNull
-    private LocalDateTime dateRegister;
+    private String address;
+    
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime dataRegister;
 }
