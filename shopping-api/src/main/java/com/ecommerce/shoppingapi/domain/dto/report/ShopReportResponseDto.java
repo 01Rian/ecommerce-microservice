f939d6f8552f422805b384a5a8d1632ef7069c65
Rian <riansantosjr11@gmail.com>
2025-02-24 15:13:03 -0300

    (shopping-api): rename ShopReportDto

---
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/domain/dto/report/ShopReportResponseDto.java
@@ -0,0 +1,19 @@
+package com.ecommerce.shoppingapi.domain.dto.report;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ShopReportResponseDto {
+
+    private Integer count;
+    private BigDecimal total;
+    private BigDecimal mean;
+}
