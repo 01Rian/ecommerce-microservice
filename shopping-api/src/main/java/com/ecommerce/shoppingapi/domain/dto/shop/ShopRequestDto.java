8657756da929fab6ec707f202aa5459bf58cc4e9
Rian <riansantosjr11@gmail.com>
2025-02-27 17:21:18 -0300

    (shopping-api): refactor ShopRequestDto to remove unused imports and clean up code

--- shopping-api/src/main/java/com/ecommerce/shoppingapi/domain/dto/shop/ShopRequestDto.java
+++ shopping-api/src/main/java/com/ecommerce/shoppingapi/domain/dto/shop/ShopRequestDto.java
@@ -2,13 +2,11 @@ package com.ecommerce.shoppingapi.domain.dto.shop;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.Builder;
 
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
